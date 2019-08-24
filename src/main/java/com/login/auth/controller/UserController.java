d25f05857123546a5da886ad3f9220b0c6b61f4c
Litterial <yungice12@gmail.com>
2019-08-23 19:37:34 -0500

    doing template and controller for register

--- src/main/java/com/login/auth/controller/UserController.java
+++ src/main/java/com/login/auth/controller/UserController.java
@@ -1,17 +1,43 @@
 package com.login.auth.controller;
 
+import com.login.auth.models.Data.UserDAO;
+import com.login.auth.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.login.auth.models.User;
+
+import javax.validation.Valid;
+import org.springframework.validation.Errors;
 
 @Controller
+@RequestMapping(value = "")
 public class UserController {
+    @Autowired
+    private UserService userService;
     @RequestMapping(value="")
     public String index(Model model)
     {
         model.addAttribute("title","Hello World");
         return "index";
     }
+    @RequestMapping(value="register",method = RequestMethod.GET)
+    public String RegisterForm(Model model)
+    {
+        System.out.println("registerGet");
+        model.addAttribute("user",new User());
+        return "register";
+    }
+
+//    @RequestMapping(value="register", method=RequestMethod.POST)
+//    public String verifyRegister(@Valid @ModelAttribute("user") User createUser, Errors err, Model model)
+//    {
+//        System.out.println("registerPost");
+//    }
 }
 
 
